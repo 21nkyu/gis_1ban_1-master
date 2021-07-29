d7ee5e4c0e878f4a6af057d2dbb6996ecaa83fac
21nkyu <adele7178@naver.com>
2021-07-30 00:13:28 +0900

    20210728 001
    it's ok

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -1,9 +1,7 @@
 from django.forms import ModelForm
-
 from profileapp.models import Profile
 
-
 class ProfileCreationForm(ModelForm):
-    class Meta: #이 데이터는 아니지만 이것을 설명해주는 정보들
+    class Meta:
         model = Profile
-        fields = ['image', 'nickname', 'message'] #user는 서버에서 처리해주기 때문에 받지 않는다.
+        fields = ['image', 'nickname', 'message']
